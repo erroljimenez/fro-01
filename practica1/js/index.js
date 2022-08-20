4d28ae421dbf864a14c7b9b978c44d68bf641f04
erroljimenez <ejimenezal@ucenfotec.ac.cr>
2022-08-20 09:59:01 -0600

    Creacion carpeta practica 1-2-3-4
    Se agregan los archivos de la practica 1-2-3-4

---
+++ practica1/js/index.js
@@ -0,0 +1,38 @@
+
+//cree variable num1 y asignele el valor de 7
+//let num1 = 7;
+//cree variable num2 y asignele el valor 9
+//let num2 = 9;
+//cree una variable llamada resultado1 que guarde la suma de num1 + num2
+//let resultado1 =  num1 + num2;
+//imprima el resultado 1 y el resultado 2
+//console.log (resultado1);
+//cree una variable llamada resultado2 que guarde el resultado de la multiplicacion num1*num2
+//let resultado2 =  num1 * num2;
+//imprima el resultado 1 y el resultado 2
+//console.log (resultado2);
+
+let h2Nombre = document.getElementById('nombre-personaje');
+
+//para mayusculas
+h2Nombre.textContent = h2Nombre.textContent.toUpperCase ();
+
+//imprimir el texto que tiene el elemento dentro
+console.log ('Contenido: ', h2Nombre.textContent) ;
+
+console.log ('Nombre de la clase: ', h2Nombre.className);
+
+console.log ('log:', h2Nombre.id) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
